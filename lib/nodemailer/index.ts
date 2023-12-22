5fcf4eef090a79668e2c6fcce432e70aa93aee66
m-waseem <iamwaseem0066@gmail.com>
2023-12-22 15:21:49 +0530

    updated code

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -85,7 +85,7 @@ const transporter = nodemailer.createTransport({
   service: 'hotmail',
   port: 2525,
   auth: {
-    user: 'javascriptmastery@outlook.com',
+    user: 'iamwaseem0066@gmail.com',
     pass: process.env.EMAIL_PASSWORD,
   },
   maxConnections: 1
@@ -93,7 +93,7 @@ const transporter = nodemailer.createTransport({
 
 export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) => {
   const mailOptions = {
-    from: 'javascriptmastery@outlook.com',
+    from: 'iamwaseem0066@gmail.com',
     to: sendTo,
     html: emailContent.body,
     subject: emailContent.subject,
